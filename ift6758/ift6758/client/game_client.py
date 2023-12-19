1ec5787856033546c40696998c3936c33d72bdfe
Kaiqi Cheng <catchcheng@gmail.com>
2023-12-19 17:21:23 -0500

    update game_client

--- ift6758/ift6758/client/game_client.py
+++ ift6758/ift6758/client/game_client.py
@@ -1,22 +1,30 @@
-from ..tidy_data import game_client
+from ...tidy_data import generate_game_client_df
 import pandas as pd
 import numpy as np
 import requests
 
 class GameClient:
     def get_game_data(game_id: int):
-        url_game = f'https://statsapi.web.nhl.com/api/v1/game/{int(game_id)}/feed/live/'
+        url_game = f'https://api-web.nhle.com/v1/gamecenter/{int(game_id)}/play-by-play/'
         response = requests.get(url_game)
 
-        if response.status_code != 200:
-            return None
-        return response.json()
+        if response.status_code == 200:
+            data = response.json()
+            with open(url_game) as file:
+                return json.dumps(data, file)
 
     def ping_game(game_id: int) -> pd.DataFrame:
         # feature engineering, clean, transform from json to df
-        df = get_game_data(game_id: int)
+        df = get_game_data(game_id)
 
-        if data is None:
+        if df is None:
             return pd.DataFrame()
         
         return df
+
+# #Testing
+game_id_to_test = 2022020451
+client = GameClient()
+result_df = client.ping_game(game_id_to_test)
+
+print(result_df)
