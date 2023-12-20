a8307cc8d6c757e8141b53379d1c07d25740f170
Milosh Devic <milosh.devic@gmail.com>
2023-12-20 13:15:40 -0500

    updated port for serving client

--- ift6758/ift6758/client/serving_client.py
+++ ift6758/ift6758/client/serving_client.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 class ServingClient:
-    def __init__(self, ip: str = "0.0.0.0", port: int = 5000, features=None):
+    def __init__(self, ip: str = "0.0.0.0", port: int = 8000, features=None):
         self.base_url = f"http://{ip}:{port}"
         logger.info(f"Initializing client; base URL: {self.base_url}")
 
